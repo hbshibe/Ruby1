1067b54585cbb2945186e6e0839fb8a1f5351a53
EC2 Default User <ec2-user@ip-172-31-43-191.us-east-2.compute.internal>
2023-08-22 15:13:34 +0000

    potepan課題

--- memo.rb
+++ memo.rb
@@ -1,17 +1,23 @@
 require "csv"
 
 puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
-memo_type = gets.chomp.to_i
+
+memo_type = gets.to_i
 
 if memo_type == 1
   puts "拡張子を除いたファイル名を入力してください:"
   filename = gets.chomp + ".csv"
   puts "メモしたい内容を記入してください:"
-  puts "完了したらCtrl+D（またはcommand+D）を押します"
-  content = $stdin.read.chomp
-  
+  puts "完了したらcommand + Dを押します"
+  content = []
+  while (line = gets) != "\x04\n"
+    content << line.chomp
+  end
+
   CSV.open(filename, 'w') do |csv|
-    csv << [content]
+    content.each do |line|
+      csv << [line]
+    end
   end
 
   puts "メモが保存されました。"
@@ -27,11 +33,20 @@ elsif memo_type == 2
     end
     
     puts "新しい内容を記入してください:"
-    puts "完了したらCtrl+D（またはcommand+D）を押します"
-    content = $stdin.read.chomp
+    puts "完了したらcommand + Dを押します"
+    content = []
+    while (line = gets) != "\x04\n"
+      content << line.chomp
+    end
     
+    existing_content = CSV.read(filename)
     CSV.open(filename, 'w') do |csv|
-      csv << [content]
+      existing_content.each do |line|
+        csv << line
+      end
+      content.each do |line|
+        csv << [line]
+      end
     end
 
     puts "メモが更新されました。"
